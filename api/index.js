55ceec5813e0cd587f855239e5dbbe34b396bbaa
Victor Labussiere <victor.labussiere95@gmail.com>
2023-03-03 15:55:34 -0300

    API services to feed my personal web site

---
+++ api/index.js
@@ -0,0 +1,26 @@
+// Import packages
+const [cors, express, path] = ['cors', 'express', 'path'].map(i => require(i))
+const app = express()
+const router = express.Router()
+
+// Middlewares
+app.use(cors())
+
+// Endpoints
+router.get('/', (req, res) => {
+    res.json({
+        title: "API para fornecer os serviços necessários para meu portifólio."
+    })
+})
+
+router.get('/download', async (req, res) => {
+    const file = await path.join(__dirname, '../static', 'curriculo-victor.pdf')
+    res.download(file)
+})
+
+// Server
+app.use(router)
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    return console.log(`Server running at port http://localhost:${port}`)
+})
